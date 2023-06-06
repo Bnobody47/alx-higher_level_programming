2ecadf678f5007d719c55457de94beb7023dd646
Bnobody47 <bnobodygetmoney@gmail.com>
2023-06-06 12:40:35 -0700

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-#!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
 if number < 0:
     ldigit = -number % 10
     ldigit = -ldigit
