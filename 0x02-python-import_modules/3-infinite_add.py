dfb3d19d47f15c98348d3c56df117eea59dd154c
Bnobody47 <bnobodygetmoney@gmail.com>
2023-06-08 10:45:57 -0700

    Infinte

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,9 +2,6 @@
 if __name__ == "__main__":
     import sys
     sume = 0
-    i = 0
-    for a in sys.argv:
-        if i > 0:
-            sume += int(a)
-            i += 1
+    for i in range(1, len(sys.argv)):
+        sume = sume + int(sys.argv[i])
     print(sume)
